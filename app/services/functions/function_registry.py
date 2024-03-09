857507e7739782f86c2a2558196285912763a62a
Federico Gonzalez <dederico@gmail.com>
2024-03-09 13:19:21 -0600

    function_registry

--- app/services/functions/function_registry.py
+++ app/services/functions/function_registry.py
@@ -4,4 +4,4 @@ from .implementations.hangup import hangup
 from .implementations.save_selection import save_client_selection
 from .implementations.identify import get_customer_identity
 from .implementations.redirect import redirect_call
-registered_functions = [get_current_weather, hangup, save_client_selection, redirect_call]
+registered_functions = [get_current_weather, hangup, save_client_selection, redirect_call, get_customer_identity]

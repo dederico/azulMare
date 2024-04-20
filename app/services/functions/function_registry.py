ee73a063d906c0d55631082d9c4abca3246b407e
dederico <dederico@gmail.com>
2024-04-19 23:21:19 -0600

    add amazon,identity, prometheus, and routes

--- app/services/functions/function_registry.py
+++ app/services/functions/function_registry.py
@@ -1,5 +1,6 @@
 from .implementations.get_credit_card_options import get_credit_card_options
 from .implementations.weather import get_current_weather
 from .implementations.hangup import hangup
+from .implementations.identify import get_customer_identity
 
-registered_functions = [get_credit_card_options, hangup, get_current_weather]
+registered_functions = [get_credit_card_options, hangup, get_current_weather, get_customer_identity]

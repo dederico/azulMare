3ee308ed3e971e66a6838e5b61a0b0104adbbb48
dederico <dederico@gmail.com>
2024-08-13 20:22:48 -0600

    sol: cambiar de Telmex a TotalPlay

--- app/services/llm/config/system.py
+++ app/services/llm/config/system.py
@@ -20,7 +20,7 @@ system_message = """
     - Pago en llamada contigo
     - Pago en whatsapp a traves de una liga generada
     - Pago en Oxxo
-    - Pago directo en la aplicacion de Telmex
+    - Pago directo en la aplicacion de Total Play
     O cualquiera de los pagos ya conocidos por ti o por el cliente.
     
     Siempre recuerda al cliente la opcion de generar la liga de pago y enviarla por whatsapp.
@@ -74,5 +74,5 @@ system_message = """
 """
 
 hello_message = """
-    ¿Bueno?, Hola ... mi nombre es Lorena, te estamos hablando de Telmex, tenemos una promoción para liquidar tu deuda.
+    ¿Bueno?, Hola ... mi nombre es Lorena, te estamos hablando de Total Play, tenemos una promoción para liquidar tu deuda.
 """

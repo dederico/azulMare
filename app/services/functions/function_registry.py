ae29080ade3b490329a8e13005cd18ebb5888764
dederico <dederico@gmail.com>
2024-09-29 20:48:20 -0600

    feat:send_email from prompt

--- app/services/functions/function_registry.py
+++ app/services/functions/function_registry.py
@@ -4,5 +4,7 @@ from .implementations.hangup import hangup
 from .implementations.identify import get_customer_identity
 from .implementations.create_google_event import create_google_event
 from .implementations.date import get_current_date
+from .implementations.send_email import send_ticket_email
 
-registered_functions = [get_current_date, get_customer_identity, get_credit_card_options, hangup, get_current_weather, create_google_event]
+
+registered_functions = [get_current_date, get_customer_identity, get_credit_card_options, hangup, get_current_weather, create_google_event, send_ticket_email]

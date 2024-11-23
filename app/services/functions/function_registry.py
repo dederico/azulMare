fcd9a961e633af6bd990aa789bb7823775c8b0d8
dederico <dederico@gmail.com>
2024-11-23 13:03:54 -0600

    hello world

--- app/services/functions/function_registry.py
+++ app/services/functions/function_registry.py
@@ -9,4 +9,4 @@ from .implementations.save_selection import save_client_selection
 
 
 #registered_functions = [get_current_date, get_customer_identity, get_credit_card_options, hangup, get_current_weather, create_google_event] #send_ticket_email]
-registered_functions = [hangup,save_client_selection]
+registered_functions = [hangup]

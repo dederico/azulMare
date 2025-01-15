c5937250c67cf7010038962895ece0d76c6a93e1
Pedro Antonio de la Vega <vcrox@hotmail.com>
2025-01-15 14:45:44 -0600

    test

--- app/core/orchestrator.py
+++ app/core/orchestrator.py
@@ -38,7 +38,13 @@ class Orchestrator:
             await self.stt_service.set_transcript_received_callback(self.handler)
 
             async for audio_chunk in self.websocket_handler.process_stream():
-                await self.stt_service.transcribe(audio_chunk)
+                self.log(".")
+                try:
+                    self.log(f"amazon transcribe")
+                    await self.stt_service.transcribe(audio_chunk)
+                except Exception as e:
+                    self.log(f"Error al enviar evento de audio a Transcribe: {e}")
+                
             await self.stt_service.finish_transcription()
             self.log("Call ended Gracefully")
         except Exception as e:

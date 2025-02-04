4b4659b2f12c6913d8ef3b0128a008b6bd1aaa2e
Pedro Antonio de la Vega <vcrox@hotmail.com>
2025-02-03 22:37:06 -0600

    fix audio sequence

--- app/api/websocket_handler.py
+++ app/api/websocket_handler.py
@@ -335,7 +335,17 @@ class WebSocketHandler:
             rms = audioop.rms(raw_audio_data, 2)  # Calculate RMS
             # Check if the audio is loud enough and in "listening" state
             if rms > 300 and (self.switch == "listening" or self.switch is None):
-                self.playsequence.append(audio_payload)  # Append the raw payload
+                wav_buffer = io.BytesIO()
+                with wave.open(wav_buffer, 'wb') as wf:
+                    wf.setnchannels(1)  # Mono audio
+                    wf.setsampwidth(2)  # 2 bytes por muestra (16 bits)
+                    wf.setframerate(int(8000))  # Frecuencia de muestreo
+                    wf.writeframes(audio_payload)
+
+                # Obtener datos WAV desde el buffer
+                wav_buffer.seek(0)
+                wav_data = wav_buffer.read()
+                self.playsequence.append(base64.b64encode(wav_data).decode("utf-8"))  # Append the raw payload
                 return raw_audio_data
             else:
                 # Generate silence if below threshold or not in listening state
@@ -356,8 +366,8 @@ class WebSocketHandler:
         logger.debug("Received audio frame from Robot")
         if self.is_connected:
             logger.debug("Socket is connected, sending audio frame to customer")
-            byte_data = self.base64_wav_to_pcm(audio_data)
-            self.playsequence.append(byte_data)
+            # byte_data = self.base64_wav_to_pcm(audio_data)
+            self.playsequence.append(audio_data)
             await self.actions_call(self.call_sid,"playback",audio_data)
             # await self.websocket.send_json(
             #     {
@@ -392,34 +402,18 @@ class WebSocketHandler:
     
     @property
     def audio_sequence(self):
-        # return self.playsequence
-        if not self.playsequence:
-            return ""  # Evita errores si la lista está vacía
-        # Convertir a Base64 y devolverlo como string
-        audio_bytes = b"".join(self.playsequence)
-        wav_buffer = io.BytesIO()
-        with wave.open(wav_buffer, 'wb') as wf:
-            wf.setnchannels(1)  # Mono audio
-            wf.setsampwidth(2)  # 2 bytes por muestra (16 bits)
-            wf.setframerate(int(8000))  # Frecuencia de muestreo
-            wf.writeframes(audio_bytes)
+         return self.playsequence
+        # if not self.playsequence:
+        #     return [] # Evita errores si la lista está vacía
+        # audio_bytes = b"".join(self.playsequence)
+        # wav_buffer = io.BytesIO()
+        # with wave.open(wav_buffer, 'wb') as wf:
+        #     wf.setnchannels(1)  # Mono audio
+        #     wf.setsampwidth(2)  # 2 bytes por muestra (16 bits)
+        #     wf.setframerate(int(8000))  # Frecuencia de muestreo
+        #     wf.writeframes(audio_bytes)
 
-        # Obtener datos WAV desde el buffer
-        wav_buffer.seek(0)
-        wav_data = wav_buffer.read()
-
-        audio_segment = AudioSegment(
-            audio_bytes, sample_width=2, frame_rate=8000, channels=1
-        )
-
-        mp3_buffer = io.BytesIO()
-        audio_segment.export(mp3_buffer, format="mp3", bitrate="64k")  # Exportar como MP3
-
-        # Obtener los datos MP3 desde el buffer
-        mp3_buffer.seek(0)
-        mp3_data = mp3_buffer.read()
-
-        base64_audio = base64.b64encode(mp3_data).decode("utf-8")
-        # Convertir a Base64
-        # base64_audio = base64.b64encode(wav_data).decode("utf-8")
-        return base64_audio 
+        # wav_buffer.seek(0)
+        # wav_data = wav_buffer.read()
+        # base64_audio = [base64.b64encode(audio_bytes).decode("utf-8")]
+        # return base64_audio 

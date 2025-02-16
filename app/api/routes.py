601bc7c853f3108fe1a125bd467c0847acb72c0d
Pedro Antonio de la Vega <vcrox@hotmail.com>
2025-02-16 14:41:22 -0600

    deepgram

--- app/api/routes.py
+++ app/api/routes.py
@@ -112,12 +112,13 @@ async def websocket_endpoint(ws: WebSocket):
     #     enhanced=False,
     #     language=config["language"]
     # )
-    stt_task = asyncio.to_thread(AmazonTranscribeService, 
-        region="us-east-1", 
-        sample_rate=8000, 
-        enhanced=False, 
-        language=config["language"]
-    )
+    # stt_task = asyncio.to_thread(AmazonTranscribeService, 
+    #     region="us-east-1", 
+    #     sample_rate=8000, 
+    #     enhanced=False, 
+    #     language=config["language"]
+    # )
+    stt_task = asyncio.to_thread(DeepgramService, DEEPGRAM_API_KEY)
 
     function_task = asyncio.to_thread(FunctionManager, 
         registered_functions=registered_functions

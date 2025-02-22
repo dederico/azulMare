83bd80aa632f0755d1c3bd2a7018da6365678924
dederico <dederico@gmail.com>
2025-02-21 22:37:58 -0600

    feat:more knowledge base

--- app/services/functions/function_registry.py
+++ app/services/functions/function_registry.py
@@ -8,12 +8,14 @@ from .implementations.date import get_current_date
 from .implementations.save_selection import save_client_selection
 from .implementations.get_directorio import get_funcionarios
 from .implementations.get_centros_comunitarios import get_centros_comunitarios
-from.implementations.get_carta_radicacion import get_carta_radicacion
+from .implementations.get_carta_radicacion import get_carta_radicacion
 from .implementations.get_centros_bienestar import get_centros_bienestar
 from .implementations.get_centros_reciclaje import get_centros_reciclaje
 from .implementations.get_consulta_multas_transito import get_consultas_multas_transito
 from .implementations.get_denuncia_maltrato_animal import get_denuncia_maltrato_animal
 from .implementations.get_empleo import get_empleo
+from .implementations.get_parquimetros import get_parquimetros
+from .implementations.get_gimnasios import get_gimnasios
 
 registered_functions = [
     hangup,
@@ -26,6 +28,8 @@ registered_functions = [
     get_centros_reciclaje, 
     get_consultas_multas_transito,
     get_denuncia_maltrato_animal,
-    get_empleo]
+    get_empleo,
+    get_parquimetros,
+    get_gimnasios]
 
 

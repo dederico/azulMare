62d9c5412da56faa7d95458c04ddf63640c865ae
dederico <dederico@gmail.com>
2025-02-22 08:14:11 -0600

    fix:cleanup file

--- app/services/functions/function_registry.py
+++ app/services/functions/function_registry.py
@@ -1,10 +1,4 @@
-from .implementations.get_credit_card_options import get_credit_card_options
-from .implementations.weather import get_current_weather
 from .implementations.hangup import hangup
-from .implementations.identify import get_customer_identity
-from .implementations.create_google_event import create_google_event
-from .implementations.date import get_current_date
-# from .implementations.send_email import send_ticket_email
 from .implementations.save_selection import save_client_selection
 from .implementations.get_directorio import get_funcionarios
 from .implementations.get_centros_comunitarios import get_centros_comunitarios
@@ -20,7 +14,6 @@ from .implementations.get_gimnasios import get_gimnasios
 registered_functions = [
     hangup,
     save_client_selection, 
-    get_credit_card_options, 
     get_funcionarios, 
     get_centros_comunitarios, 
     get_carta_radicacion, 

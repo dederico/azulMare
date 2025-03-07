56cb406c9cf0881729384160eb47207154908329
dederico <dederico@gmail.com>
2025-03-06 20:10:58 -0600

    fix calendar

--- app/services/functions/implementations/create_google_event.py
+++ app/services/functions/implementations/create_google_event.py
@@ -30,7 +30,7 @@ logger = logging.getLogger(__name__)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 SCOPES = ['https://www.googleapis.com/auth/calendar.events','https://www.googleapis.com/auth/calendar']
 SERVICE_ACCOUNT_FILE= os.path.join(BASE_DIR, 'functions', 'service_account_credentials.json')
-CALENDAR_ID = 'adrianmgd67@gmail.com'
+CALENDAR_ID = 'c_2ef2e7be9d505f579908e7f07daab958dd3db7063c8c2ae7be001008f677d1fc@group.calendar.google.com'
 CREDENTIALS_FILE = os.path.join(BASE_DIR, 'functions', 'implementations', 'credentials.json')
 
 

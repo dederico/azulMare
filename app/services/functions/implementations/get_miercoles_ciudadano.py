af7b47f1df027dea5c0cdb15eb713769b25e20db
dederico <dederico@gmail.com>
2025-05-19 21:57:15 -0600

    fix:miercoles_ciudadano

--- app/services/functions/implementations/get_miercoles_ciudadano.py
+++ app/services/functions/implementations/get_miercoles_ciudadano.py
@@ -1,14 +1,17 @@
 TC = """
 Este documento contiene información sobre horarios y servicios brindados en el Miércoles Ciudadano
 
-Pregunta: ¿Cuándo se realizará el Miércoles Ciudadano?
-Respuesta: 12 y 26 de febrero
+Pregunta: ¿Cada cuándo se realiza el Miércoles Ciudadano?
+Respuesta: Cada 15 días
 
 Pregunta: ¿En qué horario se realiza el Miércoles Ciudadano?
 Respuesta: 11:30 am a 1:30 pm
 
 Pregunta: ¿En dónde se realiza el Miércoles Ciudadano?
 Respuesta: Bajos de Presidencia Municipal San Pedro Garza García, Juárez y Libertad S/N, Centro, 66238 San Pedro Garza García, N.L.
+
+Pregunta: ¿Dónde puedo adoptar o recibir árboles?
+Respuesta: En el Miércoles Ciudadano se realizan entregas de árboles endémicos y frutales.
 
 Pregunta: ¿Qué servicios brindan en el Miércoles Ciudadano?
 Respuesta:
@@ -21,9 +24,9 @@ Bolsa de trabajo
 """
 
 async def get_miercoles_ciudadano():
-    """Obtener Información sobre las oficialías del registro civil que se encuentran en el municipio de San Pedro Garza García en caso de ser necesario.
+    """Obtener Información sobre sobre horarios y servicios brindados en el Miércoles Ciudadano, en caso de ser necesario.
 
     Returns:
-        string: Información sobre las oficialías del registro civil que se encuentran en el municipio de San Pedro Garza García.
+        string: Información sobre sobre horarios y servicios brindados en el Miércoles Ciudadano, en el municipio de San Pedro Garza García.
     """
     return TC
